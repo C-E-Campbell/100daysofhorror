cdefbd9d3879c1c2773ad74aa8a97a47e2e14006
charles <codelyfewithcharlie@gmail.com>
2019-10-04 00:21:53 -0700

    added red span to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,10 @@ const Hero = () => {
 			<div className='hero-text-container'>
 				<div className='hero-cta-box'>
 					<h1>Can you live through the challenge?</h1>
-					<p>Tempt Fate. Take the 100 day horror movie challenge.</p>
+					<p>
+						<span className='red'>Tempt Fate. </span>Take the 100 day horror
+						movie challenge.
+					</p>
 					<a href='#'>Read More</a>
 				</div>
 			</div>
