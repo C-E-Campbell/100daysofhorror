a1ec57ed73476b245a3fd61dd4520be22dc9347a
charles <codelyfewithcharlie@gmail.com>
2019-10-05 14:19:10 -0700

    updated api stuff

--- src/Page/Login/components/MoveDescription/MovieDescription.jsx
+++ src/Page/Login/components/MoveDescription/MovieDescription.jsx
@@ -2,18 +2,41 @@ import React, { Component } from "react";
 import "./MovieDescription.style.scss";
 import axios from "axios";
 export default class MovieDescription extends Component {
+	constructor() {
+		super();
+		this.state = {
+			movie: {}
+		};
+	}
+
 	//4c3ee338
 	componentDidMount() {
 		axios
-			.get("http://www.omdbapi.com/?apikey=4c3ee338&s=nightmare+on+elm+street")
-			.then(response => console.log(response.data));
+			.get("http://www.omdbapi.com/?apikey=4c3ee338&i=tt0087800&plot=full")
+			.then(response => this.setState({ movie: response.data }));
 	}
 
 	render() {
+		const {
+			Actors,
+			Poster,
+			Director,
+			Plot,
+			Released,
+			Runtime,
+			Writer,
+			Title
+		} = this.state.movie;
 		return (
 			<div className='movie-description-box'>
-				<h1>Movie Description</h1>
-				<input onChange={e => this.setState({ searchTerm: e.target.value })} />
+				<h1>{Title}</h1>
+				<img src={Poster} alt='movie poster' />
+				<h3>{`Cast: ${Actors}`}</h3>
+				<p>{`Plot: ${Plot}`}</p>
+				<p>{`Director: ${Director}`}</p>
+				<p>{`Writer: ${Writer}`}</p>
+				<p>{`Released: ${Released}`}</p>
+				<p>{`Runtime: ${Runtime}`}</p>
 			</div>
 		);
 	}
