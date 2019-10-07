e1d33575727b0fd37b22eec744701e5a5e5bef63
charles <codelyfewithcharlie@gmail.com>
2019-10-06 20:52:03 -0700

    updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+
+import { Link } from "react-router-dom";
 import "./Header.style.scss";
 
 const Header = props => {
 	return (
 		<div className='header'>
 			<div className='header-container'>
-				<div className='header-logo'></div>
+				<Link to='/' className='header-logo'></Link>
 				<ul className='header-nav'>
 					<Link to='/login' className='header-li'>
-						Login
+						Dashboard
 					</Link>
 				</ul>
 			</div>
