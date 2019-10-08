fd8db3bfe17daef2bd21af350a62d85e55936d0d
charles <codelyfewithcharlie@gmail.com>
2019-10-08 16:36:13 -0700

    added delete method to server

--- server/controller/movie_controller.js
+++ server/controller/movie_controller.js
@@ -5,5 +5,13 @@ let newID = 101;
 module.exports = {
 	get: (req, res) => {
 		res.status(200).send(data);
+	},
+	delete: (req, res) => {
+		const { id } = req.params;
+		const index = data.findIndex(movie => {
+			return movie.id === parseInt(id);
+		});
+		data.splice(index, 1);
+		res.status(200).send(data);
 	}
 };
--- server/index.js
+++ server/index.js
@@ -3,16 +3,11 @@ const m = require("./controller/movie_controller");
 const data = require("./myAPI.json");
 const app = express();
 const cors = require("cors");
+app.use(express.json());
 
 app.get("/api/movies", cors(), m.get);
 
-app.get("/api/movie/:id", cors(), (req, res) => {
-	const { id } = req.params;
-	const index = data.findIndex(movie => {
-		return movie.id === parseInt(id);
-	});
-	res.status(200).send(data[index]);
-});
+app.delete("/api/movie/:id", cors(), m.delete);
 
 const port = 8290;
 app.listen(port, () => {
