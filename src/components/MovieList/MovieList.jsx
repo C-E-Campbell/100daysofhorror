55161966b1a8f070290172562ad0a75d3f52d5f9
charles <codelyfewithcharlie@gmail.com>
2019-10-08 14:24:47 -0700

    updates

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,24 +2,40 @@ import React, { Component } from "react";
 import "./MovieList.style.scss";
 import MovieItem from "../MovieItem/MovieItem";
 export default class MovieList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchTerm: ""
+		};
+	}
 	render() {
-		const mappedMovies = this.props.movies.map(movie => {
-			return (
-				<MovieItem
-					omdb={movie.omdbID}
-					youtube={movie.youtubeID}
-					key={movie.id}
-					id={movie.id}
-					title={movie.title}
-					myFunc={() => this.props.getID(movie)}
-				/>
-			);
-		});
+		const mappedMovies = this.props.movies
+			.map(movie => {
+				return (
+					<MovieItem
+						omdb={movie.omdbID}
+						youtube={movie.youtubeID}
+						key={movie.id}
+						id={movie.id}
+						title={movie.title}
+						myFunc={() => this.props.getID(movie)}
+					/>
+				);
+			})
+			.filter(movie => {
+				return movie.props.title
+					.toLowerCase()
+					.includes(this.state.searchTerm.toLowerCase());
+			});
 
 		return (
 			<div className='movie-list'>
 				<h2 className='movie-list-title'>Movies To Watch</h2>
-				<input placeholder='Search Movies' />
+				<input
+					value={this.state.searchTerm}
+					placeholder='Search Movies'
+					onChange={e => this.setState({ searchTerm: e.target.value })}
+				/>
 				{mappedMovies}
 			</div>
 		);
