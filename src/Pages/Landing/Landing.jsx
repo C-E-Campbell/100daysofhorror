26d51552c1b5dc32dd87c9df1ea80264c12695a8
charles <codelyfewithcharlie@gmail.com>
2019-11-26 13:03:42 -0700

    updates

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import Hero from "../../Components/Hero/Hero";
+import Hero from "../../components/Hero/Hero";
 import About from "../../Components/About/About";
 import AskUs from "../../Components/AskUs/AskUs";
 import Footer from "../../Components/Footer/Footer";
 class Landing extends React.Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-	render() {
-		return (
-			<div>
-				<Hero />
-				<About />
-				<AskUs />
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Hero />
+        <About />
+        <AskUs />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Landing;
