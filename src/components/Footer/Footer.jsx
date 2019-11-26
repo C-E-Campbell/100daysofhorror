b74f34f10649fd5bdca99279c9efdbb6cf1ecc7b
charles <codelyfewithcharlie@gmail.com>
2019-11-26 13:17:29 -0700

    updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
-import React from "./node_modules/react";
+import React from "react";
 import "./Footer.style.scss";
 
 const Footer = () => {
-	return (
-		<div className='footer' id='footer'>
-			<div className='footer-container'>
-				<a
-					href='https://charlescampbell.dev'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					View My Portfolio
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-container">
+        <a
+          href="https://charlescampbell.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View My Portfolio
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
