4f207fce26f4121c18559820455867e260732f2a
charles <codelyfewithcharlie@gmail.com>
2019-12-31 22:29:09 -0500

    added ui photo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.style.scss";
 import { Link } from "react-scroll";
+import ui from "../../Assets/assets/ui.jpg";
 class About extends React.Component {
   constructor(props) {
     super(props);
@@ -22,35 +23,42 @@ class About extends React.Component {
   };
   render() {
     return (
-      <div
-        id="toRules"
-        className="about"
-        data-aos="fade"
-        style={{ backgroundPositionY: this.state.offset / 2 }}
-      >
+      <div className="about-flex">
         <h2>Challenge Rules</h2>
-        <div className="about-container">
-          <div className="about-item">Two difficulties: Easy and MurderMe.</div>
-          <div className="about-item">
-            Depending on the difficulty, you will have a set timeline.
+        <div
+          id="toRules"
+          className="about"
+          data-aos="fade"
+          style={{ backgroundPositionY: this.state.offset / 2 }}
+        >
+          <div className="about-container">
+            <div className="about-item">
+              Two difficulties: Easy and MurderMe.
+            </div>
+            <div className="about-item">
+              Depending on the difficulty, you will have a set timeline.
+            </div>
+            <div className="about-item">
+              Watch all the movies on the list before challenge ends.
+            </div>
+            <div className="about-item">
+              If you can complete the challenge you will receive an SVG to
+              display anywhere.
+              <br /> Post it as a badge of honor! Or... a badge of gore.
+            </div>
+            <div className="about-item">
+              Note: For now, Completing this challenge is merely an integrity
+              check.
+            </div>
+            <div className="about-item red">I can't stalk ALL of you...</div>
           </div>
-          <div className="about-item">
-            Watch all the movies on the list before challenge ends.
+          <div>
+            <img className="ui-img" src={ui} alt="ui" />
           </div>
-          <div className="about-item">
-            If you can complete the challenge you will receive an SVG to display
-            anywhere.
-            <br /> Post it as a badge of honor! Or... a badge of gore.
-          </div>
-          <div className="about-item">
-            Note: For now, Completing this challenge is merely an integrity
-            check.
-          </div>
-          <div className="about-item red">I can't stalk ALL of you...</div>
+          <Link duration={700} smooth={true} to="askus">
+            <i id="pulse" className="fas fa-arrow-alt-circle-down fa-lg"></i>
+          </Link>
         </div>
-        <Link duration={700} smooth={true} to="askus">
-          <i id="pulse" className="fas fa-arrow-alt-circle-down fa-lg"></i>
-        </Link>
       </div>
     );
   }
